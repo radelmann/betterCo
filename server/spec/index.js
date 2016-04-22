744cb0aa7ff1b0447146792fb73783e7c45ee565
radelmann <robadelmann@gmail.com>
2016-04-22 11:21:05 -0700

    added test conditions for post route

--- server/spec/index.js
+++ server/spec/index.js
@@ -22,17 +22,26 @@ describe("better company api - Server - REST API Routes", function() {
     it('responds with a 200 (successful) and returned json object of new comment post', function(done) {
 
       var data = {
-          message: 'this is a new message',
+          message: 'this is a test post',
           userName: 'spec test user'
         };
 
+
+    //     {
+    //   "_id": "571a6444939171020690c98c",
+    //   "message": "this is a test post",
+    //   "userName": "radelmann",
+    //   "__v": 0
+    // }
+    
       request(app)
         .post('/api/post/')
         .send(data)
         .expect(function(res) {
           expect(res.body).to.exist;
-          //expect(res.body.id).to.exist;
-          //expect(res.body.name).to.equal('new_user_api_test');
+          expect(res.body._id).to.exist;
+          expect(res.body.message).to.equal('this is a test post');
+          expect(res.body.userName).to.equal('spec test user');
         })
         .expect(200, done);
     });
